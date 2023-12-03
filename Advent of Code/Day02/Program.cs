978233147729726c7bcd134ae9ff5ad2595e0e38
svknoe <33001939+svknoe@users.noreply.github.com>
2023-12-03 09:28:35 +0100

    Day03

---
+++ Advent of Code/Day02/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Drawing;
+using System.Text.RegularExpressions;
+
+var colors = new[] { "red", "green", "blue" };
+var regexDictionary = colors.ToDictionary(c => c, c => new Regex($"(?<= )(\\d+)(?= {c})"));
+var lines = File.ReadAllLines("data.txt").ToList();
+
+var answerA = PartA();
+var answerB = PartB();
+return;
+
+int PartA()
+{
+    var gameRegex = new Regex("(?<=Game )(.*)(?=:)");
+    var limitDictionary = new Dictionary<string, int> { { "red", 12 }, { "green", 13 }, { "blue", 14 } };
+
+    return lines.Sum(line => colors.Any(color => HasTooManyCubes(line, color)) ? 0 : int.Parse(gameRegex.Match(line).Value));
+
+    bool HasTooManyCubes(string line, string color) { return regexDictionary[color].Matches(line).Any(match => int.Parse(match.Value) > limitDictionary[color]); }
+}
+
+int PartB()
+{
+    var powerSum = 0;
+
+    foreach (var line in lines)
+    {
+        var linePower = 1;
+
+        foreach (var color in colors)
+        {
+            var matches = regexDictionary[color].Matches(line).Select(x => int.Parse(x.Value)).ToList();
+            var maxColorIncidence = matches.Any() ? matches.Max() : 0;
+            linePower *= maxColorIncidence;
+        }
+
+        powerSum += linePower;
+    }
+
+    return powerSum;
+}
