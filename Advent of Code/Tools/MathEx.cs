c5140dd5a1c5b9d1db0691920ea93cd8a288abe0
svknoe <33001939+svknoe@users.noreply.github.com>
2023-12-07 17:39:40 +0100

    remove EqualDoubles()
    There are no doubles in aoc

--- Advent of Code/Tools/MathEx.cs
+++ Advent of Code/Tools/MathEx.cs
@@ -8,19 +8,6 @@ namespace Tools
 {
     public static class MathEx
     {
-        public static bool EqualDoubles(double a, double b, double tol = 1e-9)
-        {
-            return (a - tol <= b && b <= a + tol)
-                   || (double.IsPositiveInfinity(a) && double.IsPositiveInfinity(b))
-                   || (double.IsNegativeInfinity(a) && double.IsNegativeInfinity(b))
-                   || (double.IsNaN(a) && double.IsNaN(b));
-        }
 
-        public static bool EqualDoubles(double? a, double? b, double tol = 1e-9)
-        {
-            if (a is double doubleA && b is double doubleB) return EqualDoubles(doubleA, doubleB, tol);
-            if (a is null && b is null) return true;
-            return false;
-        }
     }
 }
