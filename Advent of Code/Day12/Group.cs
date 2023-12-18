97e686336c5151a8527ebacddc484c11cd9ed966
svknoe <33001939+svknoe@users.noreply.github.com>
2023-12-18 22:53:25 +0100

    day 12 wip

---
+++ Advent of Code/Day12/Group.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day12
+{
+    public class Group
+    {
+        public List<char> Springs { get; set; } = new();
+        public List<int> DamagedGroupSizes { get; set; } = new();
+
+        public override string ToString()
+        {
+            var asString = "";
+            Springs.ForEach(x => asString += x);
+
+            return asString;
+        }
+
+        public int GetArrangementCount()
+        {
+
+        }
+    }
+}
--- Advent of Code/Day12/Program.cs
+++ Advent of Code/Day12/Program.cs
@@ -1,2 +1,69 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Day12;
+// ReSharper disable ParameterTypeCanBeEnumerable.Local
+
+var lines = File.ReadAllLines("data.txt").ToList();
+
+foreach (var line in lines)
+{
+    var split = line.Split(' ');
+    var springString = split[0];
+    var damagedGroupSizes = split[1].Split(',').Select(int.Parse).ToList();
+
+    var groups = InitialiseGroups(springString);
+
+    var groupStrings = groups.Select(g => g.Springs.ToList()).ToList();
+    FillGroups(groups, damagedGroupSizes);
+    for (var i = 0; i < groups.Count; i++) groups[i].Springs = groupStrings[i];
+
+}
+
+return;
+
+List<Group> InitialiseGroups(string springString)
+{
+    var groups = new List<Group>();
+    var group = new Group();
+    while (springString.Any())
+    {
+        if (springString.First() == '.')
+        {
+            if (group.Springs.Any())
+            {
+                groups.Add(group);
+                group = new Group();
+            }
+
+            springString = springString.Remove(0, 1);
+        }
+        else
+        {
+            group.Springs.Add(springString.First());
+            springString = springString.Remove(0, 1);
+        }
+    }
+
+    if (group.Springs.Any()) groups.Add(group);
+
+    return groups;
+}
+
+void FillGroups(List<Group> groups, List<int> damagedGroupSizes)
+{
+    var groupQueue = new Queue<Group>(groups);
+    var group = groupQueue.Dequeue();
+
+    var size = 0;
+    foreach (var t in damagedGroupSizes)
+    {
+        size = t;
+        while (group.Springs.Count < size) group = groupQueue.Dequeue();
+
+        group.DamagedGroupSizes.Add(size);
+        group.Springs.RemoveRange(0, Math.Min(size + 1, group.Springs.Count));
+    }
+
+    if (groupQueue.Any(g => g.Springs.Count >= size))
+    {
+
+    }
+}
